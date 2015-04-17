51e3974afffa962ef0247d4a942ad61600116cbd
Zachary Davison <zac.developer@gmail.com>
2015-04-17 13:07:52 +0100

    Added Carthage support via dynamic framework target

---
+++ MGImageUtilities/MGImageUtilities.h
@@ -0,0 +1,19 @@
+//
+//  MGImageUtilities.h
+//  MGImageUtilities
+//
+//  Created by Zachary Davison on 17/04/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MGImageUtilities.
+FOUNDATION_EXPORT double MGImageUtilitiesVersionNumber;
+
+//! Project version string for MGImageUtilities.
+FOUNDATION_EXPORT const unsigned char MGImageUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MGImageUtilities/PublicHeader.h>
+
+
